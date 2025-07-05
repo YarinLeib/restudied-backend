e2cd1fe8eae85e24fc8799928ba912143e22dfd6
Yarin Leib <yarinleib@gmail.com>
2025-07-05 16:19:50 +0300

    remove report

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 //tesst
 
-const { notFoundHandler, errorHandler } = require('./middleware/errorHandler');
+const { notFoundHandler, errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 5005;
@@ -14,13 +14,11 @@ app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.use('/api/items', require('./routes/item.routes'));
-app.use('/api/users', require('./routes/user.routes'));
-app.use('/api/reviews', require('./routes/review.routes'));
-app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/messages', require('./routes/messages.routes'));
-
-app.use('/api/reports', require('./routes/report.routes'));
+app.use("/api/items", require("./routes/item.routes"));
+app.use("/api/users", require("./routes/user.routes"));
+app.use("/api/reviews", require("./routes/review.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/messages", require("./routes/messages.routes"));
 
 // Error handling middleware
 app.use(notFoundHandler);
@@ -30,11 +28,11 @@ app.use(errorHandler);
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
   });
