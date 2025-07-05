b1b8a3a101ec5eb667c6ca3aff6d2169217dc0b4
Yarin Leib <yarinleib@gmail.com>
2025-07-05 16:44:59 +0300

    users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,16 @@ const User = require("../models/User.model");
 const Item = require("../models/Item.model");
 
 const router = express.Router();
-
+// GET /api/users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching all users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 // GET /api/users/profile
 router.get("/profile", isAuthenticated, async (req, res) => {
   try {
